d2e1a561319128c074c72c2956cc95808ad5248e
eWloYW8 <3171132517@qq.com>
2025-10-15 14:08:16 +0800

    feat: remove submission cleanup logic for deleted problems during reload

--- internal/api/admin/management.go
+++ internal/api/admin/management.go
@@ -3,10 +3,7 @@ package admin
 import (
 	"fmt"
 	"net/http"
-	"strconv"
-	"strings"
 
-	"github.com/ZJUSCT/CSOJ/internal/config"
 	"github.com/ZJUSCT/CSOJ/internal/database/models"
 	"github.com/ZJUSCT/CSOJ/internal/judger"
 	"github.com/ZJUSCT/CSOJ/internal/util"
@@ -47,78 +44,6 @@ func (h *Handler) reload(c *gin.Context) {
 		return
 	}
 
-	// Process and delete submissions for removed problems
-	deletedCount := 0
-	for _, sub := range allSubmissions {
-		if _, exists := newProblemIDs[sub.ProblemID]; !exists {
-			// This submission's problem was removed. Time to delete the submission.
-			zap.S().Infof("problem %s for submission %s was removed, preparing to delete submission", sub.ProblemID, sub.ID)
-
-			// For running submissions, we must clean up the container and resources first.
-			if sub.Status == models.StatusRunning {
-				h.appState.RLock()
-				problem, ok := h.appState.Problems[sub.ProblemID]
-				h.appState.RUnlock()
-
-				if !ok {
-					zap.S().Warnf("problem definition for running submission %s (problem %s) not found in old state during reload. Cannot stop container or release resources cleanly. DB record will be deleted anyway.", sub.ID, sub.ProblemID)
-				} else {
-					// This logic is adapted from the interrupt handler.
-					var dockerCfg config.DockerConfig
-					var nodeCfgFound bool
-					for _, clusterCfg := range h.cfg.Cluster {
-						if clusterCfg.Name == sub.Cluster {
-							for _, nodeCfg := range clusterCfg.Nodes {
-								if nodeCfg.Name == sub.Node {
-									dockerCfg = nodeCfg.Docker
-									nodeCfgFound = true
-									break
-								}
-							}
-							break
-						}
-					}
-
-					if !nodeCfgFound {
-						zap.S().Errorf("node config '%s'/'%s' not found for sub %s, cannot stop container", sub.Cluster, sub.Node, sub.ID)
-					} else {
-						docker, err := judger.NewDockerManager(dockerCfg)
-						if err != nil {
-							zap.S().Errorf("failed to connect to docker on node %s for sub %s cleanup: %v", sub.Node, sub.ID, err)
-						} else {
-							for _, container := range sub.Containers {
-								if container.DockerID != "" {
-									zap.S().Infof("forcefully cleaning up container %s for deleted submission %s", container.DockerID, sub.ID)
-									docker.CleanupContainer(container.DockerID)
-								}
-							}
-						}
-					}
-					// Parse allocated cores from submission record to release them
-					var coresToRelease []int
-					if sub.AllocatedCores != "" {
-						coreStrs := strings.Split(sub.AllocatedCores, ",")
-						for _, s := range coreStrs {
-							coreID, err := strconv.Atoi(s)
-							if err == nil {
-								coresToRelease = append(coresToRelease, coreID)
-							}
-						}
-					}
-					h.scheduler.ReleaseResources(problem.Cluster, sub.Node, coresToRelease, problem.Memory)
-				}
-			}
-
-			// Hard delete the submission from the database.
-			if err := h.db.Delete(&models.Submission{}, sub.ID).Error; err != nil {
-				zap.S().Errorf("failed to delete submission %s during reload: %v", sub.ID, err)
-			} else {
-				deletedCount++
-				zap.S().Infof("deleted submission %s because its problem %s was removed", sub.ID, sub.ProblemID)
-			}
-		}
-	}
-
 	// Create new Problem-to-Contest map
 	newProblemToContestMap := make(map[string]*judger.Contest)
 	for _, contest := range newContests {
@@ -136,8 +61,7 @@ func (h *Handler) reload(c *gin.Context) {
 	zap.S().Info("app state reloaded successfully")
 
 	util.Success(c, gin.H{
-		"contests_loaded":     len(newContests),
-		"problems_loaded":     len(newProblems),
-		"submissions_deleted": deletedCount,
+		"contests_loaded": len(newContests),
+		"problems_loaded": len(newProblems),
 	}, "Reload successful")
 }
